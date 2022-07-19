b9fc464242f13cc589a4deea9a33cdcc19e8b1f9
OvieDev <swiderskijan40@gmail.com>
2022-07-19 17:31:17 +0200

    Added .env file with the token

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
-
 import discord
+from dotenv import load_dotenv
 from discord.ext import commands
 
+load_dotenv()
 bot = commands.Bot(command_prefix="rush!")
+
+
 
 bot.start(os.getenv("TOKEN"))
